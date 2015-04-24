e2ba53989ae96a438c7a9e6312701686ed844739
hometue <hometue.lee@gmail.com>
2015-04-24 20:08:38 +0800

    Made major changes to the classes to allow minimal (or no) direct write to the class variable. No change in functionality, the collision detection remains faulty and commented out.

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
+#Player
 class Player:
-    def __init__(self,x1,y1):
-        self.x=x1
-        self.y=y1
+    def __init__(self,x,y, surface):
+        self.x=x
+        self.y=y
+        self.surface=surface
         self.speed=5
-    def pos(self, x1,y1):
-        self.x=x1
-        self.y=y1
+    def changepos(self, changex, changey):
+        self.pos(self.x+changex, self.y+changey)
+    def pos(self, x,y):
+        self.x=x
+        self.y=y
         self.draw()
     def draw(self):
-        rect = guyimg.get_rect()
+        rect = self.surface.get_rect()
         rect.topleft = (self.x, self.y)
-        screen.blit(guyimg, rect)
-
+        screen.blit(self.surface, rect)
+#Solid objects
 class Wall:
-    def __init__(self,x1,y1):
-        self.x=x1
-        self.y=y1
+    def __init__(self,x,y, surface):
+        self.x=x
+        self.y=y
+        self.surface=surface
         self.draw()
     def draw(self):
-        wall1=wallimg.get_rect()
+        wall1=self.surface.get_rect()
         wall1.topleft=(self.x, self.y)
-        screen.blit(wallimg, wall1)
+        screen.blit(self.surface, wall1)
         
 # INTIALISATION
 import pygame, math, sys
 from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-guyimg = pygame.image.load('img/player.png')
-wallimg = pygame.image.load('img/wall.png')
 clock = pygame.time.Clock()
 k_up = k_down = k_left = k_right = 0
-x=y=100 #spawn location
 BLACK = (0,0,0)
-me=Player(x,y)
-newwall=Wall(0,0)
-textfont=pygame.font.SysFont("arial", 12)
+me=Player(100,100, pygame.image.load('img/player.png'))
+newwall=Wall(0,0, pygame.image.load('img/wall.png'))
+textfont=pygame.font.SysFont("arial", 12) #test code for now, leave here for text printing
 while 1:
     # USER INPUT
     clock.tick(30)
@@ -46,16 +48,12 @@ while 1:
         elif event.key == K_LEFT: k_left = down * -me.speed
         elif event.key == K_UP: k_up = down * -me.speed
         elif event.key == K_DOWN: k_down = down * me.speed
-        elif event.key == K_ESCAPE: pygame.quit ()# quit the game
+        elif event.key == K_ESCAPE: pygame.quit()# quit the game
         #if event.type == pygame.QUIT: pygame.quit ()
     screen.fill(BLACK)
     
-    # SIMULATION
-    x += k_left+k_right
-    y += k_down+k_up
-    # RENDERING
-    me.pos(x, y)
-    #if((guyimg.get_rect()).colliderect(wallimg.get_rect())):
+    me.changepos(k_left+k_right, k_down+k_up) #move the player to the new location
+    #if((me.surface.get_rect()).colliderect(newwall.surface.get_rect())==True):
     #    print("Collided")
     newwall.draw()
     
