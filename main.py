33078ab650a2c4bc7d67d6ca48fedd15b15c0e32
hometue <hometue.lee@gmail.com>
2015-04-30 15:00:03 +0800

    New additions: A collision system. NOTE: Collision system is WIP. It has a serious flaw in the logic at the moment.

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ class Player:
     def pos(self, x,y):
         self.x=x
         self.y=y
-        self.draw()
-    def draw(self):
         self.rect.topleft = (self.x, self.y)
+    def draw(self):
         screen.blit(self.surface, self.rect)
 #Solid objects
 class Wall:
@@ -48,7 +47,7 @@ k_up = k_down = k_left = k_right = 0
 BLACK = (0,0,0)
 listitem=[Item(50,100, pygame.image.load('img/item.png')), Item(200,200, pygame.image.load('img/item.png'))]
 me=Player(100,100, pygame.image.load('img/player.png'))
-newwall=Wall(0,0, pygame.image.load('img/wall.png'))
+walllist=[Wall(0,0, pygame.image.load('img/wall.png')), Wall(300,300, pygame.image.load('img/wall.png'))]
 textfont=pygame.font.SysFont("arial", 12) #test code for now, leave here for text printing
 while 1:
     # USER INPUT
@@ -65,10 +64,22 @@ while 1:
     screen.fill(BLACK)
     currentx=me.x
     currenty=me.y
-    me.changepos(k_left+k_right, k_down+k_up) #move the player to the new location
-    if me.rect.colliderect(newwall.rect)==True:
-        print("Collided")
-    newwall.draw()
+    me.changepos(k_left+k_right, 0) #move the player to the new location
+    x=len(walllist)-1
+    while x>=0:
+        if me.rect.colliderect(walllist[x].rect)==True:
+            me.pos(walllist[x].x+1+walllist[x].rect.width,me.y)
+        x-=1
+    x=len(walllist)-1
+    me.changepos(0, k_down+k_up) #move the player to the new location
+    while x>=0:
+        if me.rect.colliderect(walllist[x].rect)==True:
+            me.pos(me.x,walllist[x].y+1+walllist[x].rect.height)
+        x-=1
+    x=len(walllist)-1
+    while x>=0:
+        walllist[x].draw()
+        x-=1
     x=len(listitem)-1
     while x>=0:
         listitem[x].draw()
@@ -79,5 +90,5 @@ while 1:
             else:
                 x-=1
         else: x-=1
-    
+    me.draw()
     pygame.display.flip()
